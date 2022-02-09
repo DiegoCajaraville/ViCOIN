07ab18e34e7eb2ae8386beba0339ebfe19f592c2
Santiago Cerviño Sánchez <santi832000@gmail.com>
2022-02-09 22:32:51 +0100

    Puesta a punto del entorno truffle para hacerlo mas amigable
    Estoy trabajando en hacer ficheros de pruebas que se encontrarán en la carpeta tests y se ocuparán de desplegar el contrato y hacer las pruebas que se indiquen automáticamente. Además quiero que al desplegar el contrato se muestren de una manera mas amigable los métodos, su llamada y si son pagables, etc.

---
+++ backend/test/ViCOIN_tests.js
@@ -0,0 +1,20 @@
+const ViCOIN = artifacts.require("ViCOIN")
+
+contract("ViCOIN", () => {
+
+    before( async () => {
+        this.ViC = await ViCOIN.deployed()
+    })
+
+    it('migrate deployed successfully', async () => {
+        const address = this.ViC.address
+
+        console.log(address);
+        console.error(this.ViC);
+        assert.notEqual(address,null);
+        assert.notEqual(address, undefined);
+        assert.notEqual(address, 0x0);
+        assert.notEqual(address, '');
+    })
+
+})
