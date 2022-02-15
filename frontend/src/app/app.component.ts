7d9fc9c8915b70e16edc31a980863a71b95de1a5
dvdcordeiro <davidcordeiro12@gmail.com>
2022-02-15 18:55:22 +0100

    david

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+
+import { Component, ViewChild } from '@angular/core';
+//import {MapInfoWindow, MapMarker} from '@angular/google-maps';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor() {}
+
+  title = 'Como usar el Componente Google Maps ';
+
+  // Configuración de Google Maps
+  center = {lat: 24, lng: 12};
+  zoom = 15;
+  display?: google.maps.LatLngLiteral;
+
 }
+
+/*@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})*/
+
