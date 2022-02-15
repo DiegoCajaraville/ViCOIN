7bde1a9183536345570e4d8d147cb4d6bdc3eade
javieroteromosquera <javierotero12043@gmail.com>
2022-02-15 19:04:16 +0100

    javi
    aa

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,26 +1,10 @@
-
-import { Component, ViewChild } from '@angular/core';
-//import {MapInfoWindow, MapMarker} from '@angular/google-maps';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-
-  title = 'Como usar el Componente Google Maps ';
-
-  // Configuración de Google Maps
-  center = {lat: 24, lng: 12};
-  zoom = 15;
-  display?: google.maps.LatLngLiteral;
-
-}
-
-/*@Component({
-  selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
-})*/
-
+})
+export class AppComponent {
+  constructor() {}
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,19 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
- 
-// Google Maps de Angular 9 
-import {GoogleMapsModule} from '@angular/google-maps'; 
- 
-import { AppComponent } from './app.component';
- 
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, GoogleMapsModule,],
-  providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
-
+export class AppModule {}
