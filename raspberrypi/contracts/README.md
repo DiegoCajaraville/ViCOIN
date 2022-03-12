b750fc6c84e908a3beb33f57e44405227deb9a9b
Santiago Cerviño Sánchez <santi832000@gmail.com>
2022-03-12 19:09:18 +0100

    creado script python para consultar el método de la blockchain remaining desde la raspi

---
+++ raspberrypi/contracts/README.md
@@ -0,0 +1,4 @@
+Direcciones de la primera version de los contratos desplegados sobre la testnet Ropsten:
+- ViCOINSale:  `0x548Dd29243fE0253Bd182A743133c109a6CA9E7A`
+- ViCOIN: `0xF4Acd5dFD9b3b9fbDA1FEd81aaBA6AF5a9F6A26A`
+- Tarifas: `0x9c289B7EF6B4b25E927810B8f4EccBE61e6F400e`
---
+++ raspberrypi/getTimePatinete.py
@@ -0,0 +1,16 @@
+from web3 import Web3
+import json,sys
+
+DIR_CONTRATO_TARIFAS = '0x9c289B7EF6B4b25E927810B8f4EccBE61e6F400e'
+URI_INFURA = '7cf06df7347d4670a96d76dc4e3e3410'
+
+#INFURA HTTP API
+infura_url = 'https://ropsten.infura.io/v3/' + URI_INFURA  # your uri
+w3 = Web3(Web3.HTTPProvider(infura_url))
+
+with open('contracts/Tarifas.json') as json_file:
+    info_json = json.load(json_file)
+    abi = info_json['abi']
+    contract = w3.eth.contract(address=DIR_CONTRATO_TARIFAS,abi=abi)
+    print(contract.functions.remaining(int(sys.argv[1])).call())
+
